28ad523627fd764cec2ceaff9c655cf8f0286763
Bobby Neal <rsneal01@gmail.com>
2020-05-07 17:23:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.each do |name, value|
-    if 
+  # name_hash.each do |name, value|
+    if name_hash[:blake] < name_hash[:ashley] && name_hash[:blake]
 end
