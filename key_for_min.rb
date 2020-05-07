cfe51d2303431e09c4aee2e10bfa20d629b0be9b
Bobby Neal <rsneal01@gmail.com>
2020-05-07 17:29:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,11 @@
 def key_for_min_value(name_hash)
   name_hash.each do |name, value|
     if name_hash[:blake] < name_hash[:ashley] && name_hash[:blake] < name_hash[:adam]
-      return name
+      return :blake
     elsif name_hash[:ashley] < name_hash[:blake] && name_hash[:ashley] < name_hash[:adam]
-      return name
+      return :ashley
     elsif name_hash[:adam] < name_hash[:blake] && name_hash[:adam] < name_hash[:ashley]
-      return name
+      return :adam
     end
   end
 end
