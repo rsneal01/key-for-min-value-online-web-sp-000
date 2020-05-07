fdf900f5f00a348849d4802620323d1e2e63dbfc
Bobby Neal <rsneal01@gmail.com>
2020-05-07 18:14:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,18 +1,31 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
-  # name_hash.each do |name, value|
-    if name_hash[:blake] < name_hash[:ashley] && name_hash[:blake] < name_hash[:adam]
-      return :blake
-    elsif name_hash[:ashley] < name_hash[:blake] && name_hash[:ashley] < name_hash[:adam]
-      return :ashley
-    elsif name_hash[:adam] < name_hash[:blake] && name_hash[:adam] < name_hash[:ashley]
-      return :adam
+  lowest_key = nil
+  lowest_value = nil
+  name_hash.each do |name, value|
+    if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      lowest_key = name
     end
-    # return nil
-  
+  end
+  return lowest_key
 end
+
+
+
+# def key_for_min_value(name_hash)
+#   # name_hash.each do |name, value|
+#     if name_hash[:blake] < name_hash[:ashley] && name_hash[:blake] < name_hash[:adam]
+#       return :blake
+#     elsif name_hash[:ashley] < name_hash[:blake] && name_hash[:ashley] < name_hash[:adam]
+#       return :ashley
+#     elsif name_hash[:adam] < name_hash[:blake] && name_hash[:adam] < name_hash[:ashley]
+#       return :adam
+#     end
+#     # return nil
+  
+# end
 
 
 # def key_for_min_value(name_hash)
